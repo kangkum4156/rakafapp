f46938e16c51cc7dd018b074d4b6a09166f0c6cc
kmsso <kangkum4156@naver.com>
2025-03-27 13:49:04 +0900

    bottom navigation bar version

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -58,7 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.map),
           onPressed: () {
             Navigator.pushReplacement(
               context,
